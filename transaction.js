96ea082549476164aaeb4cd9cac600ea1800dab0
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-11-10 15:01:43 -0500

    Create transaction module with Transaction class.

--- transaction.js
+++ transaction.js
@@ -2,3 +2,38 @@
 
 import {assets} from './asset.js';
 import {getAssetId} from './asset.js';
+
+//Define and export Transaction Class
+export class Transaction 
+{
+    constructor(assetId, type, quantity)
+    {
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+        this.apply();
+    }
+}
+
+apply()
+{
+    if (this.type === "buy")
+    {
+        this.asset.quantity += this.quantity;
+    }
+    else if (this.type === "sell")
+    {
+        if (this.asset.quantity >= this.quantity)
+        {
+            this.asset.quantity -= this.quantity;
+        }
+        else 
+        {
+            throw new error('Insufficient quantity for sale of ${this.asset.name}');
+        }
+    }
+    else 
+    {
+        throw new error('Type must be "buy" or "sell"');
+    }
+};
