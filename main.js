fc1c833be8b3cef0039aac5221128dfa9fdd8e16
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-11-10 15:26:00 -0500

    Implement main application and log portfolio details and transactions.

--- main.js
+++ main.js
@@ -5,3 +5,35 @@ import {getPortfolioAllocation} from "./portfolio.js";
 import {Transaction} from "./transaction.js";
 
 //Display total portfolio value and allocation percentages
+function displayPortfolio()
+{
+    const totalValue = calculatePortfolioValue();
+    console.log("Total Portfolio Value: $" + totalValue);
+
+    const allocationPercentage = getPortfolioAllocation();
+    console.log("Percent of portfolio value: $" + allocationPercentage);
+}
+
+
+//Transaction Instances
+
+function processTransaction()
+{
+    try
+    {
+        new Transaction(DIS, "buy", 20);
+        new Transaction(F, "sell", 50);
+        new Transaction(MAR, "buy", 100);
+    }
+    catch (error)
+    {
+        console.log("There was an error")
+    }
+}
+
+console.log("Portfolio Before Transactions:");
+displayPortfolio();
+
+processTransaction();
+console.log("Portfolio After Transaction:")
+displayPortfolio();
